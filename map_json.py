7d93f5a208fa36145dbc2dd40cbf23d83950222f
Will Bruey <willbruey@gmail.com>
2018-12-31 05:49:29 +0000

    i can now create javascript html output from lat lon

--- map_json.py
+++ map_json.py
@@ -13,20 +13,19 @@ with open('hikes.json') as hikes_json_file:
 
 
 def marker_text(lat,lon,title,text):
-    return '\n\rL.marker(['+str(lat)+', '+str(lon)+']).addTo(mymap).bindPopup("<b>'+str(title)+'</b><br />'+str(text)+'");\n\r'
+    return 'L.marker(['+str(lat)+', '+str(lon)+']).addTo(mymap).bindPopup("<b>'+str(title)+'</b><br />'+str(text)+'");'
     
 
 def circle_text(lat,lon,title,text):
-    return '\n\rL.circle(['+str(lat)+', '+str(lon)+'], 700, { color: \'red\',  fillColor: \'#f03\', fillOpacity: 0.2 }).addTo(mymap).bindPopup("<b>Hello world!</b><br />Hike Me Reb.");\n\r'
+    return 'L.circle(['+str(lat)+', '+str(lon)+'], 700, { color: \'red\',  fillColor: \'#f03\', fillOpacity: 0.2 }).addTo(mymap).bindPopup("<b>'+title+'</b><br />'+text+'");'
 
 os.system('cp leaflet_map_template.html newest_map.html')
 time.sleep(2)
 
 with open("newest_map.html", "ab") as myfile:
-    pass
-    #myfile.write(marker_text(3,2,'hey','you'))
-    #myfile.write(circle_text(1,4,'hey','you'))
-
+    myfile.write(marker_text(40.11,-76.302,'hey','you'))
+    myfile.write(circle_text(40.11,-76.302,'hey','jew'))
+    myfile.write('</script> </body> </html>')
 
 
 
